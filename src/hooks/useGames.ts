030b05641ff731ede65eb8742e79200033a71046
Akash Thapa <magarthapa32@gmail.com>
2023-08-12 17:51:42 -0500

    showloading skeletons

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -24,19 +24,26 @@ const useGames = () => {
   const [games, setGames] = useState<Game[]>([]);
 
   const [error, setError] = useState('');
+  const [isLoading, setLoadding] = useState(false);
+
   useEffect(() => {
     const controller = new AbortController();
+    setLoadding(true);
     apiClient
       .get<FetchGamesResponse>('/games', { signal: controller.signal })
-      .then((res) => setGames(res.data?.results))
+      .then((res) => {
+        setGames(res.data?.results);
+        setLoadding(false);
+      })
       .catch((err) => {
         if (err instanceof CanceledError) return;
         setError(err.message);
+        setLoadding(false);
       });
     return () => controller.abort();
   }, []);
 
-  return { games, error };
+  return { games, error, isLoading };
 };
 
 export default useGames;
